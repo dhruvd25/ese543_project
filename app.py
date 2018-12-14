63664407eb808d00a19c50a7a4805a82de3e13f5
Dhruv Desai <dhruv.d25@gmail.com>
2018-12-13 22:49:08 -0500

    added lines for zipped files

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import dash_table_experiments as dt
+import zipfile
 
 # Load in data set
+zip = zipfile.ZipFile(r'output.xlsx.zip')  
+zip.extractall()  
+
 sheet_to_df_map = pd.ExcelFile('output.xlsx')
 dropdown_options = pd.read_excel('output.xlsx', sheet_name=None)
 
